769e6fe0a268e1fbc3de5441adceb111a0faec6e
Yashjain1623 <jainyash07412@gmail.com>
2024-08-24 16:59:51 +0530

    Initial commit

--- src/components/Selection.js
+++ src/components/Selection.js
@@ -1,1 +1,18 @@
 
+import React, { useState } from 'react';
+
+const Selection = ({ applyColor }) => {
+  const [backgroundStyle, setBackgroundStyle] = useState({ background: "" });
+
+  return (
+    <div
+      className="fix-box"
+      style={backgroundStyle}
+      onClick={() => applyColor(setBackgroundStyle)}
+    >
+      {/* Optional: You can add content or leave it empty */}
+    </div>
+  );
+};
+
+export default Selection;
